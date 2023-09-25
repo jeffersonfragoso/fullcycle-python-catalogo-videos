add3c12d58842f5fda3f8ca7482035c356734b67
Jefferson Fragoso de Souza <jeffersonfragoso@outlook.com>
2023-09-25 00:12:02 +0000

    feat: :zap: Adicionado implementação do 'find_by_id' para o CategoryDjangoRepository

--- src/core/category/infra/django_app/repositories.py
+++ src/core/category/infra/django_app/repositories.py
@@ -1,4 +1,5 @@
 from typing import List
+from core.__seedwork.domain.exceptions import NotFoundException
 from core.__seedwork.domain.value_objects import UniqueEntityID
 from core.category.domain.entities import Category
 from core.category.domain.repositories import CategoryRepository
@@ -11,7 +12,14 @@ class CategoryDjangoRepository(CategoryRepository):
         CategoryModel.objects.create(**entity.to_dict())
 
     def find_by_id(self, entity_id: str | UniqueEntityID) -> Category:
-        raise NotImplementedError()
+      model = self._get(str(entity_id))
+      return Category(
+        unique_entity_id=UniqueEntityID(str(entity_id)),
+        name=model.name,
+        description=model.description,
+        is_active=model.is_active,
+        created_at=model.created_at
+      )
 
     def find_all(self) -> List[Category]:
         raise NotImplementedError()
@@ -24,3 +32,9 @@ class CategoryDjangoRepository(CategoryRepository):
 
     def search(self, input_params: CategoryRepository.SearchParams) -> CategoryRepository.SearchResult:
       raise NotImplementedError()
+
+    def _get(self, entity_id: str) -> CategoryModel:
+      try:
+          return CategoryModel.objects.get(pk=entity_id)
+      except CategoryModel.DoesNotExist as exception:
+          raise NotFoundException(f"Entity not found using ID '{entity_id}'") from exception
