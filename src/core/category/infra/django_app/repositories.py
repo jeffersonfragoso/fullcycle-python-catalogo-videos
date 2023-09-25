388625779009fbdaf04f19e901f80a01b721601c
Jefferson Fragoso de Souza <jeffersonfragoso@outlook.com>
2023-09-25 00:20:00 +0000

    test: :zap: Adicionado testes para 'find_by_id' do CategoryDjangoRepository

--- src/core/category/infra/django_app/repositories.py
+++ src/core/category/infra/django_app/repositories.py
@@ -1,4 +1,5 @@
 from typing import List
+from django.core import exceptions as django_exceptions
 from core.__seedwork.domain.exceptions import NotFoundException
 from core.__seedwork.domain.value_objects import UniqueEntityID
 from core.category.domain.entities import Category
@@ -36,5 +37,5 @@ class CategoryDjangoRepository(CategoryRepository):
     def _get(self, entity_id: str) -> CategoryModel:
       try:
           return CategoryModel.objects.get(pk=entity_id)
-      except CategoryModel.DoesNotExist as exception:
+      except (CategoryModel.DoesNotExist, django_exceptions.ValidationError) as exception:
           raise NotFoundException(f"Entity not found using ID '{entity_id}'") from exception
--- src/core/category/tests/integration/infra/django_app/test_int_repositories.py
+++ src/core/category/tests/integration/infra/django_app/test_int_repositories.py
@@ -2,6 +2,8 @@
 # pylint: disable=unexpected-keyword-arg,no-member
 
 import unittest
+from core.__seedwork.domain.exceptions import NotFoundException
+from core.__seedwork.domain.value_objects import UniqueEntityID
 import pytest
 from core.category.infra.django_app.repositories import CategoryDjangoRepository
 from core.category.infra.django_app.models import CategoryModel
@@ -44,3 +46,35 @@ class TestCategoryDjangoRepositoryInt(unittest.TestCase):
         self.assertEqual(model.description, 'Movie description')
         self.assertFalse(model.is_active)
         self.assertEqual(model.created_at, category.created_at)
+
+    def test_throw_not_found_exception_in_find_by_id(self):
+        with self.assertRaises(NotFoundException) as assert_error:
+            self.repo.find_by_id('fake id')
+        self.assertEqual(
+            assert_error.exception.args[0], "Entity not found using ID 'fake id'")
+
+        with self.assertRaises(NotFoundException) as assert_error:
+            self.repo.find_by_id('af46842e-027d-4c91-b259-3a3642144ba4')
+        self.assertEqual(
+            assert_error.exception.args[0], "Entity not found using ID 'af46842e-027d-4c91-b259-3a3642144ba4'")
+
+        unique_entity_id = UniqueEntityID(
+            'af46842e-027d-4c91-b259-3a3642144ba4')
+        with self.assertRaises(NotFoundException) as assert_error:
+            self.repo.find_by_id(unique_entity_id)
+        self.assertEqual(
+            assert_error.exception.args[0],
+            "Entity not found using ID 'af46842e-027d-4c91-b259-3a3642144ba4'"
+        )
+
+    def test_find_by_id(self):
+        category = Category(
+            name='Movie',
+        )
+        self.repo.insert(category)
+
+        category_found = self.repo.find_by_id(category.id)
+        self.assertEqual(category_found, category)
+
+        category_found = self.repo.find_by_id(category.unique_entity_id)
+        self.assertEqual(category_found, category)
